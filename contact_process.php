27231fc9400d6cc6e435d026319d8f3dfc5d9f54
rial2001 <rial2001@mail.ru>
2026-03-14 11:09:02 +0300

    правки 1

--- contact_process.php
+++ contact_process.php
@@ -1,17 +1,17 @@
+
 <?php
 
     $to = "spn8@spondonit.com";
     $from = $_REQUEST['email'];
     $name = $_REQUEST['name'];
-    $subject = $_REQUEST['subject'];
+    $form_subject = $_REQUEST['subject'];
     $number = $_REQUEST['number'];
     $cmessage = $_REQUEST['message'];
 
-    $headers = "From: $from";
-	$headers = "From: " . $from . "\r\n";
-	$headers .= "Reply-To: ". $from . "\r\n";
-	$headers .= "MIME-Version: 1.0\r\n";
-	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+    $headers = "From: " . $from . "\r\n";
+    $headers .= "Reply-To: ". $from . "\r\n";
+    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
     $subject = "You have a message from your Bitmap Photography.";
 
